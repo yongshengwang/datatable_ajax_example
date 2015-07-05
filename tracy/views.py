52f53eff56e9cae14c844946c04b88cc1727f7fa
Tiny <wysnight@gmail.com>
2015-07-05 19:52:08 +0800

    add search

--- tracy/views.py
+++ tracy/views.py
@@ -5,7 +5,7 @@ import json
 # Create your views here.
 from django.template import RequestContext
 from tracy.models import User
-
+from django.db.models import Q
 def test(request):
     return render_to_response('test.html', locals(), context_instance=RequestContext(request))
 
@@ -14,16 +14,23 @@ def get_data(request):
     order_dict = {'asc':'','desc':'-'}
     order = order_list[int(request.GET.get('order[0][column]', '0'))]
     order_dir = order_dict[request.GET.get('order[0][dir]','asc')]
-    print request.GET.get('search[value]','')
+    search =  request.GET.get('search[value]','')
+    if search:
+        search_q = Q(name__contains=search) | Q(age__contains=search)
     start = request.GET.get('start',0)
     end = start + request.GET.get('length',10)
-    print end
-    t = User.objects.all().order_by(order_dir+order)
+    total = User.objects.all().count()
+    if search:
+        t = User.objects.filter(search_q).order_by(order_dir+order)
+        filter_total = User.objects.filter(search_q).count()
+    else:
+        t = User.objects.all().order_by(order_dir+order)
+        filter_total = total
     users = t[start:end]
     return HttpResponse(json.dumps(
         {"draw": int(request.GET['draw']),
-         "recordsTotal": len(t),
-         "recordsFiltered": len(t),
+         "recordsTotal": total,
+         "recordsFiltered": filter_total,
          'data':[{'name': u.name, 'age': u.age, 'link':{'action': get_action(u.id), 'id': u.id}} for u in users]},
         cls=DjangoJSONEncoder))
 
